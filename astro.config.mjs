6eb2e1e82c0536460223b31da8c4abd5f6b13fbf
Shreesh Kulkarni <shreesh@Shreeshs-MacBook-Pro.local>
2026-02-22 19:17:32 -0500

    SEO integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://shreeshkulkarni.dev',
   integrations: [
     tailwind({
       // We manage our own CSS file with @tailwind directives
